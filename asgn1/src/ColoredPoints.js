a3386462cb3bb7fc8ed284d2d84489922135cb33
804115 <jkoo9@ucsc.edu>
2025-04-13 21:37:52 -0700

    initial files

---
+++ asgn1/src/ColoredPoints.js
@@ -0,0 +1,149 @@
+// ColoredPoint.js (c) 2012 matsuda
+// Vertex shader program
+var VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  uniform float u_Size;
+  void main() {
+    gl_Position = a_Position;
+    gl_PointSize = u_Size;
+  }
+`;
+  
+// Fragment shader program
+var FSHADER_SOURCE = `
+  precision mediump float;
+  uniform vec4 u_FragColor;
+  void main() {
+    gl_FragColor = u_FragColor;
+  }
+`;
+// Global variables
+let canvas, gl, a_Position, u_FragColor, u_Size;
+let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
+let g_selectedSize = 5;
+
+function setupWebGL(){
+  // Retrieve <canvas> element
+  canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+}
+
+function connectVariablesToGLSL(){
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // Get the storage location of a_Position
+  a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return;
+  }
+
+  // Get the storage location of u_FragColor
+  u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+  if (!u_FragColor) {
+    console.log('Failed to get the storage location of u_FragColor');
+    return;
+  }
+
+  u_Size = gl.getUniformLocation(gl.program, 'u_Size');
+  if (!u_Size) {
+    console.log('Failed to get the storage location of u_Size');
+    return;
+  }
+}
+
+function addActionsHTML(){
+  // Buttons
+  document.getElementById('green').onclick = function(){ g_selectedColor = [0.0, 1.0, 0.0, 1.0]; };
+  document.getElementById('red').onclick = function(){ g_selectedColor = [1.0, 0.0, 0.0, 1.0]; };
+  document.getElementById('clearButton').onclick = function(){ g_shapesList = []; renderAllShapes(); };
+
+  //Sliders
+  document.getElementById('redSlider').addEventListener('mouseup', function(){ g_selectedColor[0] = this.value/100; });
+  document.getElementById('greenSlider').addEventListener('mouseup', function(){ g_selectedColor[1] = this.value/100; });
+  document.getElementById('blueSlider').addEventListener('mouseup', function(){ g_selectedColor[2] = this.value/100; });
+
+  document.getElementById('sizeSlider').addEventListener('mouseup', function(){ g_selectedSize = this.value; });
+}
+
+function main() {
+  setupWebGL();
+
+  connectVariablesToGLSL();
+
+  addActionsHTML();
+
+  // Register function (event handler) to be called on a mouse press
+  canvas.onmousedown = click;
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+
+var g_shapesList = [];
+/*var g_points = []; 
+var g_colors = []; 
+var g_sizes = [];*/
+function click(ev) {
+
+  let [x, y] = convertCoordinates(ev);
+
+  let point = new Point();
+  point.position = [x, y];
+  point.color = g_selectedColor.slice();
+  point.size = g_selectedSize;
+  g_shapesList.push(point);
+
+  /* Store the coordinates to g_points array
+  g_points.push([x, y]);
+
+  g_colors.push(g_selectedColor.slice());
+
+  // Store the coordinates to g_points array
+  if (x >= 0.0 && y >= 0.0) {      // First quadrant
+    g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
+  } else if (x < 0.0 && y < 0.0) { // Third quadrant
+    g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
+  } else {                         // Others
+    g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
+  }
+  
+  g_sizes.push(g_selectedSize);*/
+
+  renderAllShapes();
+}
+
+function convertCoordinates(ev){
+  var x = ev.clientX; // x coordinate of a mouse pointer
+  var y = ev.clientY; // y coordinate of a mouse pointer
+  var rect = ev.target.getBoundingClientRect();
+
+  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+
+  return [x, y];
+}
+
+function renderAllShapes(){
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var len = g_shapesList.length;
+  for(var i = 0; i < len; i++) {
+    g_shapesList[i].render();
+  }
+}
